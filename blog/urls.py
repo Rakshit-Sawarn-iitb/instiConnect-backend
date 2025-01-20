284a4ba029d02122856efbaab8593db7a44a1797
Aditya Adhana <adityaadhana0409@gmail.com>
2025-01-20 23:22:23 +0530

    adi

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from . import views
+from django.contrib import admin
+
+from .views import *
 
 urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
+    path('', get_many_blog, name='get_all'),
+    path('post/', post_blog, name='post_blog_'),
+    path('find/' , get_one_blog , name = 'get_one'),
+    path('delete/' , delete_blog , name = 'delete'),
+    path('find/' , get_one_blog , name = 'get_one'),
+    path('like/' , like_blog , name = 'like'),   
+    
 ]
